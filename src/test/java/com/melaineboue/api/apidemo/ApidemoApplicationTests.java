383724633f56b9c898975d0e5d7489a18226aa39
Boue Melaine <melaine.boue@etudiant.univ-rennes1.fr>
2019-12-12 06:18:12 +0100

    first commit

---
+++ src/test/java/com/melaineboue/api/apidemo/ApidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melaineboue.api.apidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
